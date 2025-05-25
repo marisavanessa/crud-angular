97fe9f6ab21e09ab55e379f63800dd102eeacc58
Marisa Vanessa N. Cruz <ideiaselembrancas@gmail.com>
2025-05-24 21:27:02 -0300

    alteração proxy

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,8 @@
 const PROXY_CONFIG = [
   {
     context: ['/api'],
-    target: 'http://localhost:8070/',
+    //target: 'http://localhost:8070/',
+    target: 'https://crud-spring-esq7.onrender.com/',
     secure: false,
     logLevel: 'debug'
   }
